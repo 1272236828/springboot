4ee2a25b28f58b76a00abd2e920bd470f8e83259
feather_lin <1034740570@qq.com>
2022-10-13 19:13:20 +0800

    2022-10-13 17:13:00

---
+++ src/main/java/com/spring/web/controller/FileUploadController.java
@@ -0,0 +1,74 @@
+package com.spring.web.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+@Controller
+public class FileUploadController {
+    @RequestMapping("/")
+    public String fileUploadController() {
+        return "/html/index";
+    }
+
+    @RequestMapping("/upload")
+    public String upload(HttpServletRequest request,
+                         @RequestParam("description") String description,
+                         @RequestParam("file") MultipartFile file)
+            throws IllegalStateException, IOException {
+        System.out.println("文件描述："  + description);
+        if (!file.isEmpty()) {
+            String path = request.getServletContext().getRealPath("/uploadFiles/");
+            String fileName = file.getOriginalFilename();
+            File filePath = new File(path + File.separator + fileName);
+            if (!filePath.getParentFile().exists()) {
+                filePath.getParentFile().mkdirs();
+            }
+            file.transferTo(filePath);
+            System.out.println(filePath);
+        }
+        return "forward:/showDownLoad";
+    }
+
+    @RequestMapping("/showDownLoad")
+    public String showDownLoad(HttpServletRequest request, Model module) {
+        String path = request.getServletContext().getRealPath("/uploadFiles/");
+        File fileDir = new File(path);
+        File fileList[] = fileDir.listFiles();
+        module.addAttribute("filesList", fileList);
+        return "/html/showFile";
+    }
+
+    @RequestMapping("/download")
+    public ResponseEntity<byte[]> download(
+            HttpServletRequest request,
+            @RequestParam("filename") String filename,
+            @RequestHeader("User-Agent") String userAgent
+    ) throws IOException {
+        String path = request.getServletContext().getRealPath("uploadFiles/");
+        File downloadFile = new File(path + File.separator + filename);
+        BodyBuilder builder = ResponseEntity.ok();
+        builder.contentLength(downloadFile.length());
+        builder.contentType(MediaType.APPLICATION_OCTET_STREAM);
+        filename = URLEncoder.encode(filename, "UTF-8");
+        if (userAgent.indexOf("MSIE") > 0) {
+            builder.header("Content-Disposition", "attachment;filename=" + filename);
+        } else {
+            builder.header("Content-Disposition", "attachment; filename=" + filename);
+        }
+        return builder.body(FileUtils.readFileToByteArray(downloadFile));
+    }
+
+}
