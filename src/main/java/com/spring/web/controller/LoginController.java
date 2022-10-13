9a8d2246413d8aa6639a0c2fafd43b0ceb4173f4
feather_lin <1034740570@qq.com>
2022-10-13 16:47:10 +0800

    2022-10-13 16:47:00

--- src/main/java/com/spring/web/controller/LoginController.java
+++ src/main/java/com/spring/web/controller/LoginController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FirstController {
-    @RequestMapping("/")
-    public String firstController(){
-        return "html/index";
+public class LoginController {
+    @RequestMapping("login")
+    public String loginController() {
+        return "html/login";
     }
 }
