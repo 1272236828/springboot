bb4db69cfe74d5ac9c4b486918224b7c659ea6c4
feather_lin <1034740570@qq.com>
2022-10-15 19:45:27 +0800

    2022-10-15 11:40:15

--- src/main/java/com/spring/web/repository/SQLUserRepositoryImpl.java
+++ src/main/java/com/spring/web/repository/SQLUserRepositoryImpl.java
@@ -15,14 +15,15 @@ public class SQLUserRepositoryImpl implements SQLUserRepository{
     private JdbcTemplate jdbcTemplate;
     @Override
     public int test(SQLUser sqlUser) {
-        String sql = "CREATE TABLE IF NOT EXISTS UploadFiles(" +
+        String FileSQL = "CREATE TABLE IF NOT EXISTS UploadFiles(" +
                 "FileID INT NOT NULL AUTO_INCREMENT," +
                 "FileName VARCHAR(255) NOT NULL," +
                 "FileSHA256 VARCHAR(255) NOT NULL," +
                 "FilePath VARCHAR(255) NOT NULL," +
                 "PRIMARY KEY (FileID)" +
                 ")";
-        jdbcTemplate.execute(sql);
+
+        jdbcTemplate.execute(FileSQL);
         return 0;
     }
     @Override
